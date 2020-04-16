52588b11280fd328f2744ddf776fd8b17f9a5318
Sergey Bulavintsev <bulavintsev.sergey@gmail.com>
2020-04-16 11:10:55 +0300

    Clean up site

--- _site/README.md
+++
@@ -1,8 +0,0 @@
-# sbulav.github.io
-
-Personal blog based on [Minimal Mistakes](https://github.com/mmistakes/minimal-mistakes).
-
-
-## Local development
-
-Use [Makefile](/Makefile)
--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "Initial post",
-        "excerpt":"I’ve always wanted to start a blog, and finally I had some time to investigate github pages and create this awesome blog. We’ll see if I’ll have enought time and courage to constantly write posts, but I have serious intention to do so. I’ll start by reponting my linkedin certification’s...","categories": ["blog"],
-        "tags": ["thoughts","update"],
-        "url": "http://0.0.0.0:8080/blog/initial-post/",
-        "teaser": null
-      }]
